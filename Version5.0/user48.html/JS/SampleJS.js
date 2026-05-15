3ab5b33bc64acf1d0f86dde4c1a735dce2478815
Im a real door <237628@shakopeeschools.org>
2026-05-15 10:19:31 -0500

    i dont know...

---
+++ Version5.0/user48.html/JS/SampleJS.js
@@ -0,0 +1,18 @@
+function changeText() {
+ document.getElementById("textChange").innerHTML="Thanks for liking my Webpage";
+}
+var space = " ";
+var pos = 0;
+var msg = "User 19";
+Scroll();
+function openWebsite() {
+    window.location.href = 'https://www.youtube.com/watch?v=d1NjkYjRn34'
+}
+let box = document.getElementById('counter');
+let checked = document.getElementById('counter');
+function increment() {
+    box.value = parseInt(box.value) + 1;
+}
+function timesChecked() {
+    checked.value = parseInt(checked.value) - 1 - document.getElementById('checked').value
+}
